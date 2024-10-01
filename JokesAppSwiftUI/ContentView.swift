8788a3b7ddf64d1eb3c584a629ef643163879051
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-10-01 20:56:11 +0300

    Initial Commit

---
+++ JokesAppSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokesAppSwiftUI
+//
+//  Created by Beyza Nur Tekerek on 1.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JokesAppSwiftUI/JokesAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppSwiftUIApp.swift
+//  JokesAppSwiftUI
+//
+//  Created by Beyza Nur Tekerek on 1.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
